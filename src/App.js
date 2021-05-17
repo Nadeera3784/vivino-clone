c9889aaa4058cb9cccaeb31d8e5b2605d3846a88
dev-PC\dev <nadeera3784@gmail.com>
2021-05-17 13:54:03 +0530

    feat : initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {Row, Container, Dropdown} from 'react-bootstrap';
+import {Helmet} from "react-helmet";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/font-awesome.min.css';
+import './styles/app.css';
+import './styles/checkbox.css';
+import './styles/star-rating.css';
+import 'rc-slider/assets/index.css';
+import "@fremtind/jkl-radio-button/radio-button.min.css";
+
+import DropDownProvider  from './providers/DropDownProvider';
+
+import BottomBar      from './components/Header/BottomBar';
+import TopBar         from './components/Header/TopBar';
+import Footer         from './components/Footer/Footer';
+import Products       from './components/Products/Products';
+import Sidebar        from './components/Sidebar/Sidebar';
+import FilterSortSm   from './components/Filters/FilterSortSm';
+import FilterSortMd   from './components/Filters/FilterSortMd';
+import FilterStatus   from './components/Filters/FilterStatus';
+import FilterDrawer   from './components/Filters/FilterDrawer';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_container">
+       <Helmet>
+          <title>Red Wine, above $0, rated 3.5 or more</title>
+        </Helmet>
+       <DropDownProvider>
+           <TopBar/>
+           <BottomBar/>
+       </DropDownProvider>
+       <FilterDrawer/>
+       <div className="app_wrapper">
+          <Container>
+              <Row>
+                  <FilterSortSm/>
+                  <FilterStatus/>
+                  <FilterSortMd/>
+                  <Sidebar/>
+                  <Products/>          
+              </Row>
+          </Container>
+       </div>
+      <Footer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import DrawerProvider  from './providers/DrawerProvider';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <DrawerProvider>,
+    <App/>,
+  </DrawerProvider>,
   document.getElementById('root')
 );
 
